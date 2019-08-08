9ca1bb89c19632d367aa340a316adbe992b8cc9c
spensation <spencevans@gmail.com>
2019-08-08 13:51:02 -0400

    Routes added for login, logout and sign up.  Users#New in place

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password, :password_confirmation)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+
+  get "/" => "home#index"
+  get '/sign_up' => 'users#new'
+  get '/login'   => 'sessions#new'
+  post '/login'   => 'sessions#create'
+  delete '/logout'  => 'sessions#destroy'
+  get '/logout'  => 'sessions#destroy'
+
   resources :user_songs
   resources :songs
   resources :albums
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/" => "home#index"
+
 end
