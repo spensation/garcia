34e1a2b4ca3022823ebb6ac8b0cb800ad17109d5
spensation <spencevans@gmail.com>
2019-08-15 16:33:15 -0400

    usersong#create in process

--- app/controllers/user_songs_controller.rb
+++ app/controllers/user_songs_controller.rb
@@ -1,2 +1,15 @@
 class UserSongsController < ApplicationController
+	def index
+		@usersongs = Usersongs.find_by(params[:user_id])
+	end
+
+	def create
+		@usersong = UserSong.new(usersong_params)
+	end
+
+
+	private
+	def usersong_params
+		params.require(:usersong).permit(:user_id, :song_id)
+	end
 end
