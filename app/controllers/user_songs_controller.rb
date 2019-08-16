19887dbaa1b73594fa994c8768f8cf62462a54aa
spensation <spencevans@gmail.com>
2019-08-16 12:18:33 -0400

    still trying to create usersong

--- app/controllers/user_songs_controller.rb
+++ app/controllers/user_songs_controller.rb
@@ -3,13 +3,21 @@ class UserSongsController < ApplicationController
 		@user_songs = Usersong.find_by(params[:user_id])
 	end
 
+	def new
+		@user_song = UserSong.new
+	end
 
 	def create
-		@user = current_user
-		raise params.inspect
+		@user = User.find_by(params[:id])
 
-		@user_song = UserSong.new
+		# raise params.inspect
+
+		@user_song = UserSong.new(user_song_params)
 		
 	end
 	
+	private
+    def user_song_params
+      params.require(:user_song).permit(:user_id, :song_id)
+    end
 end
